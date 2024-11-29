9d20c2e62da5909620c5ffd5848f37a59ad7694e
OmAlKml <omar.ali.kml@gmail.com>
2024-11-29 19:14:16 +0200

    Improve side drawer

--- lib/screens/drawer/side_drawer.dart
+++ lib/screens/drawer/side_drawer.dart
@@ -1,34 +1,140 @@
 import 'package:flutter/material.dart';
+import '../about/about_screen.dart';
+import '../history/history_screen.dart';
 
 class SideDrawer extends StatelessWidget {
   const SideDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ClipPath(
-      child: Drawer(
-        child: Container(
-          color: const Color(0xFF6A5ACD), // Background color of the drawer
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: const <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color(0xFF6A5ACD),
-                ),
-                child: Text(
-                  'Menu',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+    return Drawer(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black,
+              Color(0xFF333333),
             ],
           ),
         ),
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.orange,
+                    blurRadius: 10,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 70,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.orange, width: 2),
+                    ),
+                    child: const Icon(
+                      Icons.calculate_rounded,
+                      size: 40,
+                      color: Colors.orange,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Calculator',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  _buildMenuItem(
+                    icon: Icons.history,
+                    title: 'History',
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HistoryScreen(
+                            history: const [],
+                            onClearHistory: () {},
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  const Divider(color: Colors.grey),
+                  _buildMenuItem(
+                    icon: Icons.info,
+                    title: 'About',
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const AboutScreen()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                '© ${DateTime.now().year} Calculator',
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildMenuItem({
+    required IconData icon,
+    required String title,
+    required VoidCallback onTap,
+  }) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        color: Colors.orange,
+        size: 24,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+        ),
+      ),
+      onTap: onTap,
+      hoverColor: Colors.orange.withOpacity(0.1),
+      splashColor: Colors.orange.withOpacity(0.2),
+      horizontalTitleGap: 0,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
     );
   }
 }
